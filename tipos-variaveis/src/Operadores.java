1d06faf32f5c47160b13f5ed6b12795e56a7e76c
Eduardocr80 <ecrcordeirodev@hotmail.com>
2023-05-25 22:04:43 -0300

    Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,6 +1,36 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println(nomeCompleto);
+        // concatenacao
+        String concatenacao = "?";
+
+        
+
+        concatenacao = 1+1+1+"1";
+        
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        
+        System.out.println(concatenacao);
+        
+        concatenacao = "1"+(1+1+1);
+        
+        System.out.println(concatenacao);
+
+
+
+
+
+
+
     }
+
 }
