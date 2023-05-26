ea8045bf585f2469590b8c99e2ed0705f87884db
Eduardocr80 <ecrcordeirodev@hotmail.com>
2023-05-25 21:22:40 -0300

    Variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class TiposVariaveis {
+
+    public static void main(String[] args) throws Exception{
+        //estudar tipos primitivos
+        //estudar classes String que respresenta textos na aplicação
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.print(VALOR_DE_PI);
+
+
+
+
     }
+
 }
